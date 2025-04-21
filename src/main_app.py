25d176aa7065cbfbb54043070d98c8cfc477e0dc
Rockmaw <gergely.olah@gmail.com>
2025-04-22 00:42:36 +0200

    grid test

--- src/main_app.py
+++ src/main_app.py
@@ -1,5 +1,6 @@
 import dash
 from dash import dcc, html, Input, Output
+import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.graph_objects as go
 from sensor_utils import daten_von_api_holen, daten_in_datenbank_schreiben
@@ -16,6 +17,7 @@ BOX_ID = os.getenv("SENSEBOX_ID")
 
 # Dash App mit Montserrat-Font
 app = dash.Dash(__name__, external_stylesheets=[
+    dbc.themes.BOOTSTRAP,
     "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap"
 ])
 app.title = "Umweltmonitoring Dashboard"
@@ -51,232 +53,26 @@ def wind_figure(direction_deg, speed):
     return fig
 
 # Layout
-app.layout = html.Div([
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(
+            html.Div(id="temp-wert", style={"color": "black"}),md=4, xl=4, style=
+            {"backgroundColor": "aquamarine", "padding": "10px", "borderRadius": "8px", "textAlign": "center"}),
+        dbc.Col(
+            html.Div("SensorBox1",id="sensor1-box", style={"color": "blue"}),md=4, xl=4),
+            ]), 
     dcc.Interval(id="auto-update", interval=UPDATE_INTERVAL_SEKUNDEN * 1000, n_intervals=0),
     dcc.Interval(id="countdown-timer", interval=1000, n_intervals=0),
-
-    html.Div([
-        # Temperatur-Box
-        html.Div(id="temp-wert", style={
-            "width": "320px",
-            "height": "320px",
-            "padding": "20px 30px",
-            "borderRadius": "30px",
-            "backgroundColor": "#ECF0F3",
-            "boxShadow": "12px 12px 24px #d1d9e6, -12px -12px 24px #ffffff",
-            "display": "flex",
-            "flexDirection": "column",
-            "alignItems": "center",
-            "justifyContent": "center",
-            "textAlign": "center",
-            "zIndex": "1"
-        }),
-
-        # Extra-Box mit Sensor-Anzeigen
-        # Extra-Box mit Sensor-Anzeigen (üstte 3 büyük, altta 3 küçük Box)
-        html.Div([
-            # Üstteki 3 büyük kutu
-            html.Div([
-                html.Div([
-                    html.Div("Rain (hourly)", style={
-                        "position": "absolute",
-                        "top": "15px",
-                        "left": "15px",
-                        "fontSize": "16px",
-                        "fontWeight": "600",
-                        "color": "#000"
-                    }),
-                    html.Div("...", style={
-                        "zIndex": "1"
-                    })
-                ], style={
-                    "width": "350px",
-                    "height": "280px",
-                    #"marginRight": "40px",
-                    "borderRadius": "20px",
-                    "backgroundColor": "#ECF0F3",
-                    "boxShadow": "12px 12px 24px #d1d9e6, -12px -12px 24px #ffffff",
-                    "position": "relative",
-                    "display": "flex",
-                    "alignItems": "center",
-                    "justifyContent": "center"
-                }),
-                html.Div([
-                    html.Div("Wind Status", style={
-                        "position": "absolute",
-                        "top": "15px",
-                        "left": "15px",
-                        "fontSize": "16px",
-                        "fontWeight": "600",
-                        "color": "#000"
-                    }),
-                    html.Div("...", style={
-                        "zIndex": "1"
-                    })
-                ], style={
-                    "width": "350px",
-                    "height": "280px",
-                    #"marginRight": "40px",
-                    "borderRadius": "20px",
-                    "backgroundColor": "#ECF0F3",
-                    "boxShadow": "12px 12px 24px #d1d9e6, -12px -12px 24px #ffffff",
-                    "position": "relative",
-                    "display": "flex",
-                    "alignItems": "center",
-                    "justifyContent": "center"
-                }),
-                html.Div([
-                    html.Div("Pressure", style={
-                        "position": "absolute",
-                        "top": "15px",
-                        "left": "15px",
-                        "fontSize": "16px",
-                        "fontWeight": "600",
-                        "color": "#000"
-                    }),
-                    html.Div("...", style={
-                        "zIndex": "1"
-                    })
-                ], style={
-                    "width": "350px",
-                    "height": "280px",
-                    #"marginRight": "40px",
-                    "borderRadius": "20px",
-                    "backgroundColor": "#ECF0F3",
-                    "boxShadow": "12px 12px 24px #d1d9e6, -12px -12px 24px #ffffff",
-                    "position": "relative",
-                    "display": "flex",
-                    "alignItems": "center",
-                    "justifyContent": "center"
-                })
-            ], style={
-                "display": "flex",
-                "flexDirection": "row",
-                "justifyContent": "center",
-                "alignItems": "center",
-                "marginBottom": "30px",
-                "gap": "40px", 
-            }),
-
-            # Alttaki 3 küçük kutu
-            html.Div([
-                html.Div([
-                    html.Div("Humidity", style={
-                        "position": "absolute",
-                        "top": "15px",
-                        "left": "15px",
-                        "fontSize": "16px",
-                        "fontWeight": "600",
-                        "color": "#000"
-                    }),
-                    html.Div("...", style={ "zIndex": "1" })
-                ], style={
-                    "width": "350px",
-                    "height": "120px",
-                    "borderRadius": "20px",
-                    "backgroundColor": "#ECF0F3",
-                    "boxShadow": "12px 12px 24px #d1d9e6, -12px -12px 24px #ffffff",
-                    "position": "relative",
-                    "display": "flex",
-                    "alignItems": "center",
-                    "justifyContent": "center"
-                }),
-
-                html.Div([
-                    html.Div("Luminance", style={
-                        "position": "absolute",
-                        "top": "15px",
-                        "left": "15px",
-                        "fontSize": "16px",
-                        "fontWeight": "600",
-                        "color": "#000"
-                    }),
-                    html.Div("...", style={ "zIndex": "1" })
-                ], style={
-                    "width": "350px",
-                    "height": "120px",
-                    "borderRadius": "20px",
-                    "backgroundColor": "#ECF0F3",
-                    "boxShadow": "12px 12px 24px #d1d9e6, -12px -12px 24px #ffffff",
-                    "position": "relative",
-                    "display": "flex",
-                    "alignItems": "center",
-                    "justifyContent": "center"
-                }),
-
-                html.Div([
-                    html.Div("Air quality", style={
-                        "position": "absolute",
-                        "top": "15px",
-                        "left": "15px",
-                        "fontSize": "16px",
-                        "fontWeight": "600",
-                        "color": "#000"
-                    }),
-                    html.Div("...", style={ "zIndex": "1" })
-                ], style={
-                    "width": "350px",
-                    "height": "120px",
-                    "borderRadius": "20px",
-                    "backgroundColor": "#ECF0F3",
-                    "boxShadow": "12px 12px 24px #d1d9e6, -12px -12px 24px #ffffff",
-                    "position": "relative",
-                    "display": "flex",
-                    "alignItems": "center",
-                    "justifyContent": "center"
-                })
-            ], style={
-                "display": "flex",
-                "flexDirection": "row",
-                "justifyContent": "center",
-                "alignItems": "center",
-                "gap": "40px"
-            })
-        ], style={
-            "display": "flex",
-            "flexDirection": "column",
-            "justifyContent": "space-between",
-            "alignItems": "center",
-            "width": "1150px",
-            "padding": "30px",
-            "borderRadius": "30px",
-            "backgroundColor": "#ECF0F3",
-            "boxShadow": "inset 8px 8px 16px #d1d9e6, inset -8px -8px 16px #ffffff",
-            "color": "white",
-            "position": "relative",
-            "overflow": "hidden",
-            "marginLeft": "40px"
-        })
-
-    ], style={
-        "display": "flex",
-        "flexDirection": "row",
-        "alignItems": "flex-start",
-        "marginTop": "50px",
-        "marginLeft": "150px"
-    }),
-
-    html.Div(id="countdown", style={
-        "position": "fixed",
-        "bottom": "20px",
-        "left": "20px",
-        "backgroundColor": "#eeeeee",
-        "padding": "10px 15px",
-        "borderRadius": "8px",
-        "fontSize": "16px",
-        "boxShadow": "1px 1px 6px rgba(0,0,0,0.1)",
-        "color": "#333",
-        "zIndex": "999"
-    })
-], style={
-    "fontFamily": "'Montserrat', sans-serif",
-    "backgroundColor": "#ECF0F3",
-    "height": "100vh",
-    "width": "100vw",
-    "margin": "0",
-    "padding": "0",
-    "overflow": "hidden"
-})
+    html.Div("Countdown",id="countdown", style={
+            "position": "fixed",
+            "bottom": "10px",
+            "left": "10px",
+            "backgroundColor": "#222",
+            "color": "#aaa",
+            "padding": "10px",
+            "borderRadius": "8px",
+            "zIndex": "1000"})
+    ], fluid=True, class_name="mt-4 mx-4")
 
 # Temperatur-Callback
 @app.callback(
@@ -288,37 +84,19 @@ def aktualisiere_daten(n_intervals):
     daten_in_datenbank_schreiben(df, BOX_ID)
 
     if df is None or df.empty:
-        return html.Div("⚠️ Keine Daten gefunden.", style={"color": "white"})
+        return "⚠️ Keine Daten gefunden."
 
     temp_df = df[df["einheit"] == "°C"]
     if temp_df.empty:
-        return html.Div("⚠️ Keine Temperaturdaten gefunden.", style={"color": "white"})
+        return "⚠️ Keine Temperaturdaten gefunden."
 
     letzter_wert = temp_df.sort_values("zeitstempel").iloc[-1]["messwert"]
     zeit = temp_df.sort_values("zeitstempel").iloc[-1]["zeitstempel"]
     zeit_dt = pd.to_datetime(zeit).astimezone(ZoneInfo("Europe/Berlin"))
     zeit_string = zeit_dt.strftime("Stand: %d.%m.%Y – %H:%M")
-    wert_text = f"{round(letzter_wert)} °C"
+    wert_text = f"{round(letzter_wert)} °C "
 
-    return [
-        html.Div(wert_text, style={
-            "fontSize": "50px",
-            "fontWeight": "bold",
-            "color": "white"
-        }),
-        html.Hr(style={
-            "width": "80%",
-            "borderColor": "white",
-            "borderWidth": "1px",
-            "marginTop": "10px"
-        }),
-        html.Div(zeit_string, style={
-            "fontSize": "14px",
-            "color": "#aaa",
-            "marginTop": "10px",
-            "fontStyle": "italic"
-        })
-    ]
+    return wert_text
 
 # Windbox leeren
 @app.callback(
@@ -326,7 +104,7 @@ def aktualisiere_daten(n_intervals):
     Input("auto-update", "n_intervals")
 )
 def leere_windbox(n):
-    return ""
+    return "Test-Sensor1"
 
 # Countdown-Callback
 @app.callback(
